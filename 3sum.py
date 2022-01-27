d276612e3e6b4f1d106860d4d7c6320290a6993e
AbelNyc <53309012+AbelNyc@users.noreply.github.com>
2022-01-26 22:21:46 -0500

    Add files via upload

---
+++ 3sum.py
@@ -0,0 +1,22 @@
+def threeSum(nums):
+        nums.sort()
+        res = []
+        if len(nums) < 3:
+            return res
+        for i in range(len(nums) - 2):
+            if i > 0 and nums[i] == nums[i-1]: continue
+            l, r = i + 1, len(nums) - 1
+            while l < r :
+                s = nums[i] + nums[l] + nums[r]
+                if s == 0:
+                    res.append([nums[i] ,nums[l] ,nums[r]])
+                    l += 1; r -= 1
+                    while l < r and nums[l] == nums[l - 1]: l += 1
+                    while l < r and nums[r] == nums[r + 1]: r -= 1
+                elif s < 0 :
+                    l += 1
+                else:
+                    r -= 1
+        return res
+
+print(threeSum([-1,0,1,2,-1,-4]))
